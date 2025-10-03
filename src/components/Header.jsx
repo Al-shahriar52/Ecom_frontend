9d807dfd5c890174267c8c09c795f64ac4c546d0
Al-shahriar52 <shahriar15-2646@diu.edu.bd>
2025-10-03 14:15:44 +0600

    update for responsive search bar and product discount

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -310,7 +310,7 @@ const Header = () => {
                     </div>
                 </div>
 
-                <div className="header-center desktop-only">
+                <div className="header-center">
                     <form className="search-bar-wrapper" ref={searchRef} onSubmit={handleSearchSubmit}>
                         <span className="search-icon"><svg width="20" height="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M11 19C15.4183 19 19 15.4183 19 11C19 6.58172 15.4183 3 11 3C6.58172 3 3 6.58172 3 11C3 15.4183 6.58172 19 11 19Z" stroke="#888" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"></path><path d="M21 21L16.65 16.65" stroke="#888" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"></path></svg></span>
                         <input
