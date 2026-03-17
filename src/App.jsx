387ec327c8e360d7761b5c96eedc0ccccaea4ec4
Al-shahriar52 <shahriar15-2646@diu.edu.bd>
2026-03-17 11:45:59 +0600

    complete order management

--- src/App.jsx
+++ src/App.jsx
@@ -37,6 +37,7 @@ import AdminHome from './pages/admin/AdminHome';
 import ProductManagement from './pages/admin/ProductManagement';
 import UserManagement from './pages/admin/UserManagement';
 import AdminOrders from './pages/admin/orders/AdminOrders';
+import AdminOrderDetails from "./pages/admin/orders/AdminOrderDetails";
 import Accounting from './pages/admin/Accounting';
 import CouponManagement from './pages/admin/CouponManagement';
 import AddProduct from './pages/admin/AddProduct';
@@ -84,6 +85,7 @@ function App() {
                                     <Route path="users" element={<UserManagement />} />
                                     <Route path="coupons" element={<CouponManagement />} />
                                     <Route path="orders" element={<AdminOrders />} />
+                                    <Route path="orders/:orderId" element={<AdminOrderDetails />} />
                                     <Route path="accounting" element={<Accounting />} />
                                     <Route path="products/add" element={<AddProduct />} />
                                     <Route path="frequently-bought-together" element={<FbtManagementPage />} />
--- src/pages/AuthPage.jsx
+++ src/pages/AuthPage.jsx
@@ -39,7 +39,7 @@ const AuthPage = () => {
             <div className="auth-card">
                 <div className="auth-card-image">
                     <img
-                        src="https://res.cloudinary.com/dgxol8iyp/image/upload/v1769530091/ecommerce/ChatGPT_Image_Jan_27_2026_09_56_20_PM_fc2zji.png"
+                        src="https://res.cloudinary.com/dgxol8iyp/image/upload/v1772944673/authImage_yhdezq.png"
                         alt="BrightnessBeauty Logo"
                     />
                 </div>
